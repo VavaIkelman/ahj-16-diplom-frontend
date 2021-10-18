ccce46a7eadd16f00a4115e7508d850b4d385858
VavaIkelman <mr.ilkevich@gmail.com>
2021-10-18 20:36:14 +0300

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,5 @@ import Chaos from './Chaos';
 
 // const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'http://localhost:7070');
 const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://peaceful-ridge-37509.herokuapp.com');
+
 chaos.init();
