46d340469a0f34a199a392cd08fad8887f88ae26
VavaIkelman <mr.ilkevich@gmail.com>
2021-10-18 17:51:26 +0300

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Chaos from './Chaos';
 
 // const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'http://localhost:7070');
-const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://damp-depths-07664.herokuapp.com/');
+const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://gentle-refuge-32940.herokuapp.com/');
 chaos.init();
