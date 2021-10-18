fdf8c451593f90a8f83007eabdbd63b875b1f9d9
VavaIkelman <mr.ilkevich@gmail.com>
2021-10-18 19:37:21 +0300

    Init

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Chaos from './Chaos';
 
 // const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'http://localhost:7070');
-const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://agile-earth-23372.herokuapp.com/');
+const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://agile-earth-23372.herokuapp.com');
 chaos.init();
